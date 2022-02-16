290198290de9126dda3d821e02ba27cb5e84867e
Cassiano Oliveira <cassiano980@gmail.com>
2022-02-16 20:15:27 -0300

    Estrutura Base junto à Classe Turma

--- src/class/Turma.ts
+++ src/class/Turma.ts
@@ -3,12 +3,21 @@ import { Id } from "../interfaces/id";
 export class Turma implements Id{
   public id: string;
   public nome: string;
+  private docentes: string;
+  private estudantes: string;
+  private modulo?:number = 0; 
 
    constructor(
       id: string,
-      nome: string
+      nome: string,
+      docentes: string,
+      estudantes: string,
+      modulo:number
       ){
       this.id = id;
       this.nome = nome;
+      this.docentes = docentes;
+      this.estudantes = estudantes;
+      this.modulo = modulo;
    }
 }
