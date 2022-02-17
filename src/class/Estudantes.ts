699debd3082fc237ddf0ba9c8f3400866789df00
Cassiano Oliveira <cassiano980@gmail.com>
2022-02-16 22:02:47 -0300

    Estrutura básica classe Estudantes

---
+++ src/class/Estudantes.ts
@@ -0,0 +1,28 @@
+import { Turma } from "./Turma";
+import { Id } from "../interfaces/id"
+
+
+export class Estudantes implements Id{
+   public id: string;  
+   public nome: string;
+   private email: string;
+   private data_nasc: string;
+   private turma_id: string;
+   private hobbies: string;
+
+   constructor(
+      id: string,
+      nome: string,
+      email: string,
+      data_nasc: string,
+      turma_id: string,
+      hobbies: string
+      ){
+      this.id = id;
+      this.nome = nome;
+      this.email = email;
+      this.data_nasc = data_nasc;
+      this.turma_id = turma_id;
+      this.hobbies = hobbies;
+   }
+}
