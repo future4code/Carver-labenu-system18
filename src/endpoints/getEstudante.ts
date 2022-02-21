58c408be3ffcad18b59866aeb7a8fec89928c837
cesarhuber <cesar.huber@gmail.com>
2022-02-20 23:50:37 -0300

    inclusão ajustes funcionais finais

--- src/endpoints/getEstudante.ts
+++ src/endpoints/getEstudante.ts
@@ -6,18 +6,18 @@ export const getEstudantes = async(req:Request, res:Response):Promise<void> =>{
    let errorCode = 400
    try {
 
-      const hobbies = req.query.hobbie
+      const hobby = req.query.hobby
       const nome = req.query.nome
 
       let result = await connection.raw('SELECT * FROM P_labenuSystem_Estudantes')
 
-      if (hobbies) result = await connection.raw(`
-      SELECT D.nome as nome_estudante, D.data_nascimento, T.nome as turma, E.nome as hobbies
-      FROM P_labenuSystem_Docentes D
-      INNER JOIN P_labenuSystem_Estudantes_Hobbies DE ON D.id = DE.estudante_id
-      INNER JOIN P_labenuSystem_Estudantes E ON DE.estudante_id = E.id
-      INNER JOIN P_labenuSystem_Turmas T ON D.turma_id = T.id
-      WHERE E.nome = '${hobbies}'
+      if (hobby) result = await connection.raw(`
+      SELECT E.nome as nome_estudante, E.data_nascimento, T.nome as turma, H.nome as hobby
+      FROM P_labenuSystem_Estudantes E
+      INNER JOIN P_labenuSystem_Estudantes_Hobbies EH ON E.id = EH.estudante_id
+      INNER JOIN P_labenuSystem_Hobbies H ON H.id = EH.hobby_id
+      INNER JOIN P_labenuSystem_Turmas T ON E.turma_id = T.id
+      WHERE H.nome like '%${hobby}%'
       `)
 
       if (nome) result = await connection.raw(`SELECT * FROM P_labenuSystem_Estudantes WHERE nome like '%${nome}`)
